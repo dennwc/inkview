ae47eef9130e4ab642918cf91e21bcc59a290ecb
Grigoriy Efimov <efimov-gm@newpage.xyz>
2025-04-12 20:38:46 +0300

    Add simple keyboard support

--- goinkview.c
+++ goinkview.c
@@ -3,3 +3,7 @@
 int main_handler(int t, int p1, int p2) {
     return goMainHandler(t, p1, p2); 
 }
+
+void c_keyboard_handler(char* text) {
+    goKeyboardHandler(text);    
+}
---
+++ keyboard.go
@@ -0,0 +1,46 @@
+package ink
+
+/*
+#include "inkview.h"
+
+#cgo CFLAGS: -pthread
+#cgo LDFLAGS: -pthread -lpthread -linkview
+
+extern void c_keyboard_handler(char *);
+*/
+import "C"
+import (
+	"unsafe"
+)
+
+//export goKeyboardHandler
+func goKeyboardHandler(text *C.char) {
+	userKeyboardHandler(C.GoString(text))
+}
+
+type KeyboardHandler func(string)
+
+var userKeyboardHandler KeyboardHandler
+
+func SetKeyboardHandler(kh KeyboardHandler) {
+	userKeyboardHandler = kh
+}
+
+func OpenKeyboard(placeholder string, buflen int) {
+
+	if buflen <= 0 {
+		buflen = 1024
+	}
+
+	buffer := make([]byte, buflen)
+
+	ctitle := C.CString(placeholder)
+	defer C.free(unsafe.Pointer(ctitle))
+
+	cbuffer := (*C.char)(unsafe.Pointer(&buffer[0]))
+
+	var chandler C.iv_keyboardhandler
+	chandler = (C.iv_keyboardhandler)(C.c_keyboard_handler)
+
+	C.OpenKeyboard(ctitle, cbuffer, C.int(buflen), C.int(0), chandler)
+}
